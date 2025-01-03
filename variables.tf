f183759a28618333b135c2dc54defd2df71f722f
abhipost12 <abhimail12@gmail.com>
2025-01-03 13:49:26 +0100

    var changes

--- variables.tf
+++ variables.tf
@@ -1,1 +1,5 @@
-variable "instance_type" {}
+variable "instance_type" {
+  description = "The type of EC2 instance"
+  default     = "t2.micro" # optional default value
+  type        = string     # optional type specification
+}
